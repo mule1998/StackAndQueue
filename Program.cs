c8e1d1006ec9333b378ab410ce26364e58b19138
Shubham Mule <shubhambmule@gmail.com>
2021-10-22 12:54:02 +0530

    Added:Peek & Pop for Stack

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to stack and queue");
+            Console.WriteLine("Stack & Queue!");
+            StackListNode<int> stackListNode = new StackListNode<int>();
+            stackListNode.Push(70);
+            stackListNode.Push(30);
+            stackListNode.Push(56);
+            stackListNode.Show();
+            stackListNode.Pop();
+            stackListNode.Show();
+            stackListNode.Peek();
+            stackListNode.Show();
         }
     }
 }
