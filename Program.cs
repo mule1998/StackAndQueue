e4cea21a2a39fc9a935235a029790d95e6da6900
Shubham Mule <shubhambmule@gmail.com>
2021-10-22 13:15:44 +0530

    Added : Queue Created

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to stack and queue");
+            Console.WriteLine("Stack & Queue!");
+            StackListNode<int> stackListNode = new StackListNode<int>();
+            stackListNode.Push(56);
+            stackListNode.Push(30);
+            stackListNode.Push(70);
+            stackListNode.Show();
+            stackListNode.Pop();
+            stackListNode.Show();
+            stackListNode.Peek();
+            stackListNode.Show();
+            Console.WriteLine("----------------------------------------");
+            QueueListNode<int> queueListNode = new QueueListNode<int>();
+            queueListNode.Enqueue(56);
+            queueListNode.Enqueue(30);
+            queueListNode.Enqueue(70);
+            Console.WriteLine(queueListNode.Size);
+            Console.WriteLine("----------------------------------------");
+
         }
     }
 }
