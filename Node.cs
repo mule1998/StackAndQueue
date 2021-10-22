4eb2712d2a78cef9dfe0367c9cb7f2b0955efe33
Shubham Mule <shubhambmule@gmail.com>
2021-10-22 12:26:21 +0530

    Added : Created Stack with Node

---
+++ Node.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    class Node<Gtype>
+    {
+        private int data;
+        private Node<Gtype> next;
+        public Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+        public int Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+        public Node<Gtype> Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+    }
+}
---
+++ StackListNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    class StackListNode<Gtype>
+    {
+        private Node<Gtype> top;
+        public StackListNode()
+        {
+            this.top = null;
+
+        }
+        internal void Push(int d)
+        {
+            Node<Gtype> node = new Node<Gtype>(d);
+            if (this.top == null)
+                node.Next = null;
+            else
+                node.Next = this.top;
+            this.top = node;
+            Console.WriteLine($"data : {d}");
+        }
+        internal void Show()
+        {
+            Node<Gtype> temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine($"{temp.Data} ");
+                temp = temp.Next;
+            }
+        }
+    }
+}
