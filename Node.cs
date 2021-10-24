067e6bf209c29f023bf0b4449851b41ef18d034f
Shubham Mule <shubhambmule@gmail.com>
2021-10-24 12:33:01 +0530

    Added: Queue

--- Node.cs
+++ Node.cs
@@ -4,7 +4,24 @@ using System.Text;
 
 namespace StackAndQueue
 {
-    class Class1
+    class Node<Gtype>
     {
+        private int data;
+        private Node<Gtype> next;
+        public Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+        public int Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+        public Node<Gtype> Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
     }
 }
--- QueueListNode.cs
+++ QueueListNode.cs
@@ -4,7 +4,37 @@ using System.Text;
 
 namespace StackAndQueue
 {
-    class QueueListNode
+    class QueueListNode<Gtype>
     {
+        Node<Gtype> forword;
+        Node<Gtype> backword;
+        int size;
+
+        public QueueListNode()
+        {
+            this.forword = null;
+            this.backword = null;
+            size = 0;
+        }
+        internal void Enqueue(int val)
+        {
+            Node<Gtype> node = new Node<Gtype>(val);
+            if (this.backword == null)
+            {
+                this.forword = node;
+                this.backword = node;
+            }
+            else
+            {
+                this.backword.Next = node;
+                this.forword = node;
+            }
+            Console.WriteLine(val);
+            size++;
+        }
+        public int Size
+        {
+            get { return size; }
+        }
     }
 }
